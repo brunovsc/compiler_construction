9b895d591f3f839adfe1097a38ca24f17cdfbfb1
brunovsc <brunoscvieira@gmail.com>
2018-06-07 01:19:12 -0300

    Syntax Analyzer Work

---
+++ miniC/teste_comentario.c
@@ -0,0 +1,26 @@
+int main () {
+
+int x;
+// comentario de linha
+x++;
+/* 
+	aqui
+	varias
+	linhas
+	comentarios 	teste
+
+	/* 
+		aninhamento
+		de
+		comentario de bloco
+		
+		/*
+			aninhamento 3
+		*/
+	*/
+	// aninhamento de comentario de linha
+*/
+	x++;
+	return 0;
+	printf("teste string ");
+}
